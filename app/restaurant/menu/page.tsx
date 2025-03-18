7a4365645c4e142e8a76e0e435aa12e0b034f79d
Gerelzorig <51777113+Gerelzorig@users.noreply.github.com>
2025-03-18 20:33:44 +0800

    add images and add section scroll menu

--- app/restaurant/menu/page.tsx
+++ app/restaurant/menu/page.tsx
@@ -11,7 +11,11 @@ const Menu = () => {
     setActiveButton(id);
     const section = document.getElementById(id); // Use document.getElementById to get the target element
     if (section) {
-      section.scrollIntoView({ behavior: "smooth" }); // Scroll to the section smoothly
+      section.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+        inline: "nearest",
+      }); // Scroll to the section smoothly
     }
   };
 
@@ -42,51 +46,66 @@ const Menu = () => {
   }, [menu]);
 
   return (
-    <div className="pb-24 pt-16 text-white container mx-auto flex flex-col gap-4">
-      {/* <div className="animate-slide-right fixed z-10 w-8 right-4 top-1/2 -translate-y-1/2 flex flex-col gap-3 items-center">
+    <div className="pb-24 pt-16 text-white container mx-auto grid grid-cols-12 p-2 gap-4">
+      {/* Sticky Sidebar */}
+      <div className="animate-slide-left sticky top-32 self-start flex flex-col sm:col-span-4 col-span-4">
         {menu.map((category, i) => (
-          <Tooltip key={i} placement="topLeft" title={category.title}>
-            <div
-              onClick={() => handleClick(category.id)}
-              className={`${
-                activeButton === category.id
-                  ? "bg-orange-500 dark:bg-stone-400"
-                  : "bg-primary dark:bg-stone-500"
-              } w-8 h-8 p-2 cursor-pointer rounded-full overflow-hidden transition-all hover:scale-125`}
-            >
-              <img src={category.icon} alt="" />
-            </div>
-          </Tooltip>
+          <div
+            key={i}
+            onClick={() => handleClick(category.id)}
+            className={`${
+              activeButton === category.id
+                ? "bg-stone-600 dark:bg-stone-600"
+                : "bg-black dark:bg-black"
+            } cursor-pointer text-sm p-2 rounded-md`}
+          >
+            {category.title}
+          </div>
         ))}
-      </div> */}
-      {menu.map((category, i) => (
-        <div key={i} id={category.id} className="flex flex-col gap-4 mt-8">
-          <div className="font-bold text-4xl">{category.title}</div>
-          <div className="grid gap-8 grid-cols-1 sm:grid-cols-2">
-            {category.foods.map((food, i) => (
-              <div
-                key={i}
-                className="w-full flex flex-row justify-between rounded-lg overflow-hidden group"
-              >
-                {/* <div className="w-full h-80 relative overflow-hidden">
-                  <Image src={food.path} className="w-full" />
-                </div> */}
-                <div className="flex flex-col">
-                  <div className="text-2xl">{food.name}</div>
-                  <div className="text-xs">
-                    {food.description.map((description, i) => (
-                      <div className="text-extralight" key={i}>
-                        {description}
-                      </div>
-                    ))}
+      </div>
+
+      {/* Content Section */}
+      <div className="sm:col-span-8 col-span-8">
+        {menu.map((category, i) => (
+          <div key={i} id={category.id} className="flex flex-col gap-4 mb-8">
+            <div className="font-bold text-4xl">{category.title}</div>
+            <div className="grid gap-8 grid-cols-1 sm:grid-cols-2">
+              {category.foods.map((food, i) => (
+                <div
+                  key={i}
+                  className={`${
+                    food.path ? "col-span-1" : "col-span-1 sm:col-span-2"
+                  } w-full flex flex-row justify-between rounded-lg overflow-hidden group`}
+                >
+                  <div className="flex flex-col w-full">
+                    <div className="flex justify-between items-center">
+                      <div className="text-2xl">{food.name}</div>
+                      <div className="font-bold text-lg">{food.price}</div>
+                    </div>
+                    <div className="text-xs">
+                      {food.description.map((description, i) => (
+                        <div className="text-extralight" key={i}>
+                          {description}
+                        </div>
+                      ))}
+                    </div>
+                    {food.path && (
+                      <div
+                        style={{
+                          backgroundImage: `url(${food.path})`,
+                          backgroundSize: "cover",
+                          backgroundPosition: "center",
+                        }}
+                        className="w-full sm:h-80 h-64 relative overflow-hidden mt-4"
+                      ></div>
+                    )}
                   </div>
                 </div>
-                <div className="font-bold text-lg">{food.price}</div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
